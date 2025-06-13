a9d88bb304435884bce52df136fbbceecff45b82
Vũ Quốc Toàn <kytaiso1thegioi@gmail.com>
2025-06-13 22:41:42 +0700

    Add Widget ElevatedButton

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:material/widgets/drawer_header_example.dart';
+import 'package:material/widgets/elevated_button_example.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: DrawerHeaderExample(),
+      home: ElevatedButtonExample(),
     );
   }
 }
