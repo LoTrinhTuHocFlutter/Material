0d8ad73933912ddafb2434e2de4feb0bcfb2f583
Vũ Quốc Toàn <kytaiso1thegioi@gmail.com>
2025-06-13 22:25:35 +0700

    Add Widget ClipOval

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:material/widgets/circular_progress_indicator_example.dart';
+import 'package:material/widgets/clip_oval_example.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: CircularProgressIndicatorExample(),
+      home: ClipOvalExample(),
     );
   }
 }
