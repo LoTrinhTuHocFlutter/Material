b89aaa3f160fc35c25700622882f2ea652887937
Vũ Quốc Toàn <kytaiso1thegioi@gmail.com>
2025-06-13 22:40:32 +0700

    Add Widget DrawerHeader

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:material/widgets/drawer_example.dart';
+import 'package:material/widgets/drawer_header_example.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: DrawerExample(),
+      home: DrawerHeaderExample(),
     );
   }
 }
