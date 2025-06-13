0f7f09e0040535ec0fce6a4f8b5891d5dbecfa41
Vũ Quốc Toàn <kytaiso1thegioi@gmail.com>
2025-06-13 22:28:38 +0700

    Add Widget CloseButton

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:material/widgets/clip_r_rect_example.dart';
+import 'package:material/widgets/close_button_example.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: ClipRRectExample(),
+      home: CloseButtonExample(),
     );
   }
 }
