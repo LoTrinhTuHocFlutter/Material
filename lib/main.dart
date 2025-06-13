965d5b188145788824588285775273813e129573
Vũ Quốc Toàn <kytaiso1thegioi@gmail.com>
2025-06-14 06:52:53 +0700

    Add Widget TextButton

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:material/widgets/material_button_example.dart';
+import 'package:material/widgets/text_button_example.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: MaterialButtonExample(),
+      home: TextButtonExample(),
     );
   }
 }
