421c0db80b18a387ba5a8d5afb1bfc34d1ba1433
Vũ Quốc Toàn <kytaiso1thegioi@gmail.com>
2025-06-14 07:19:20 +0700

    Add Widget SimpleDialog

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:material/widgets/selectable_text_example.dart';
+import 'package:material/widgets/simple_dialog_example.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: SelectableTextExample(),
+      home: SimpleDialogExample(),
     );
   }
 }
