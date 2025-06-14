a61ce400a7b50cc1879844e4f809714653593824
Vũ Quốc Toàn <kytaiso1thegioi@gmail.com>
2025-06-14 07:07:35 +0700

    Add Widget Placeholder

---
+++ lib/widgets/placeholder_example.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class PlaceholderExample extends StatelessWidget {
+  const PlaceholderExample({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Container(
+          color: Colors.orange.shade200,
+          width: 200.0,
+          height: 200.0,
+          child: Placeholder(
+            color: Colors.black,
+            strokeWidth: 1.0,
+          ),
+        ),
+      ),
+    );
+  }
+}
