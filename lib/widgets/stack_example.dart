229662bb09bb0ad9c4dc1e3902d59dfd8e2b219e
Vũ Quốc Toàn <kytaiso1thegioi@gmail.com>
2025-06-14 07:25:49 +0700

    Add Widget Stack

---
+++ lib/widgets/stack_example.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class StackExample extends StatelessWidget {
+  const StackExample({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Padding(
+          padding: EdgeInsets.symmetric(
+            horizontal: 10.0,
+          ),
+          child: Stack(
+            children: [
+              Container(
+                width: 500.0,
+                height: 500.0,
+                color: Colors.red,
+              ),
+              Container(
+                width: 200.0,
+                height: 200.0,
+                color: Colors.green,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
