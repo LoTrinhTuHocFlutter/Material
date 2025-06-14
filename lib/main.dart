b97a2424e9c35b2279b3c67dd509124894244850
Vũ Quốc Toàn <kytaiso1thegioi@gmail.com>
2025-06-14 07:04:25 +0700

    Add Widget OutlinedButton

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:material/widgets/opacity_example.dart';
+import 'package:material/widgets/outlined_button_example.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: OpacityExample(),
+      home: OutlinedButtonExample(),
     );
   }
 }
